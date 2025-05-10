3461ef34b684d0bd1b2ca009173056bda0a3087c
ridhoauliama97 <ridhoauliama97@gmail.com>
2025-05-10 21:22:16 +0700

    1:23:29

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import assets from "../assets/assets";
 
-const RightSidebar = () => {
+const RightSidebar = ({ selectedUser }) => {
   return (
-    <div>
-      <h1>RightSidebar</h1>
-    </div>
-  )
-}
+    selectedUser && (
+      <div>
+        <div>
+          <img
+            src={selectedUser?.profilePic || assets.avatar_icon}
+            alt=""
+            className=" w-20 aspect-[1/1] rounded-full"
+          />
+          <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
+            <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            {selectedUser.fullName}
+          </h1>
+          <p className="px-10 mx-auto">{selectedUser.bio}</p>
+        </div>
+      </div>
+    )
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
